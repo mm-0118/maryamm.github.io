67529bc2c48e4b4a231b7397e66a3468157567be
mm-0118 <maryam@sokomali.com>
2025-09-04 03:12:24 -0500

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
+    document.body.addEventListener("click", function(event) {  
+        const clickedElement = event.target;
+        if (clickedElement.id) {
+            document.getElementById("blogPostText").textContent = "hi!";
+      }
+
+  });
 
 });
